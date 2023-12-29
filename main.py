fb3f27a804dc6da59e9cb8cfc539e751e17df5d0
Peter Huynh <peterhyh@yahoo.com>
2023-12-28 20:30:58 -0600

    buttons added with grid layout

--- main.py
+++ main.py
@@ -17,13 +17,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 background_img = PhotoImage(file="background.png")
 
 title = Label(text="Timer", bg=YELLOW, font=("Ariel", 50), fg=GREEN)
-title.pack()
+title.grid(column=1, row=0)
 
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=background_img)
 canvas.create_text(100, 130, text="00:00", fill="white",
                    font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
